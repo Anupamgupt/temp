5b2e9a5149f29d650f0d829e61d08c9d0d5110b6
anup kumr <akgjune06@gmail.com>
2022-04-30 16:03:13 +0530

    first commit

---
+++ temp.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+typedef vector<int>  VI;
+typedef pair<int,int> PII;
+
+#define MX INT_MAX
+#define MN INT_MIN
+// typedef "\n"  end;
+
+void solve(){
+
+}
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+   
+    return 0;
+}
