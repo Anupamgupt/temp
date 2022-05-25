539f4b800831847ba9c5b81a55fdd2b002cf55f7
Anupam Kumar Gupta <91828258+Anupamgupt@users.noreply.github.com>
2022-05-25 17:32:37 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,19 +1,28 @@
 #include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
 #define ll long long
+#define fast  ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+#define deb(a) cout<<a<<'\n'
+#define deb2(a,b) cout<<a<<" "<<b<<'\n'
 typedef vector<int>  VI;
 typedef pair<int,int> PII;
 
 #define MX INT_MAX
 #define MN INT_MIN
-// typedef "\n"  end;
+
 
 void solve(){
+   
+
 
 }
 
-int main(){
+int main(){ 
+    fast;
     ll t;
     cin>>t;
     while(t--){
